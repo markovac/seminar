a6c5b931244b93878ab8e8c7ad2b33deb5177dcf
markovac <tm50053@fer.hr>
2020-05-07 19:16:42 +0200

    Modified arp topology

--- run.sh
+++ run.sh
@@ -15,12 +15,12 @@ himage Alice echo "" > /dev/null 2>&1
 [ $? -ne 0 ] && exit 4
 
 himage router1 usr/sbin/ngctl -f- <<EOF
-    mkpeer igb0: netflow lower iface0
-    name igb0:lower netflow
-    connect igb0: netflow: upper out0
+    mkpeer eth0: netflow lower iface0
+    name eth0:lower netflow
+    connect eth0: netflow: upper out0
     mkpeer netflow: ksocket export9 inet/dgram/udp
     name netflow:export9 exporter
-    msg netflow: setconfig {iface=0 conf=7}
+    msg netflow: setconfig { iface=0 conf=7 }
     msg netflow:export9 connect inet/10.0.0.1:4444
 EOF
 
