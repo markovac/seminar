3b1c5964c8e46528d22c8c6ec79a7ffca210f4a2
markovac <tm50053@fer.hr>
2020-05-07 22:09:46 +0200

    Complete configuration script with pkg files

--- prerun.sh
+++ prerun.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# Load netgraph modules
 kldload netgraph ng_netflow ng_ether ng_ksocket
+
+# Remove cached pkg files and download libtool and nfdump
+#rm /var/cache/pkg/* > /dev/null 2>&1
+#pkg fetch libtool > /dev/null 2>&1
+#pkg fetch nfdump > /dev/null 2>&1
+
+#mv /var/cache/pkg/* temp/
+
+echo "done"
--- run.sh
+++ run.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
-BIN=/usr/local/bin/
-NFDUMP="$BIN"nfdump
-NFCAPD="$BIN"nfcapd
-LIB=/usr/local/lib/
-DEP="$LIB"libnfdump-1.6.19.so
-
-[ -f $"NFDUMP" ] && exit 1
-[ -f $"NFCAPD" ] && exit 2
-[ -f $"DEP" ] && exit 3
-
 himage Collector echo "" > /dev/null 2>&1
 
-[ $? -ne 0 ] && exit 4
+if [ $? -ne 0 ]
+then
+	echo "execute arp.imn"
+	exit 4
+fi
 
 # Configure collector
-hcp "$NFDUMP" Collector:"$BIN"
-hcp "$NFCAPD" Collector:"$BIN"
-hcp "$DEP" Collector:"$LIB"
-himage Collector mkdir /usr/netflow
-himage Collector nfcapd -S 1 -w -t 3600 -D -l /usr/netflow/ -p 4444 
+himage Collector mkdir cache > /dev/null 2>&1
+hcp temp/* Collector:/cache/
+
+himage Collector pkg add cache/m4-1.4.18_1,1.txz > /dev/null 2>&1
+himage Collector pkg add cache/indexinfo-0.3.1.txz > /dev/null 2>&1
+himage Collector pkg add cache/rrdtool-1.7.2_2.txz > /dev/null 2>&1
+himage Collector pkg add cache/libtool-2.4.6_1.txz > /dev/null 2>&1
+himage Collector pkg add cache/nfdump-1.6.19.txz > /dev/null 2>&1
+
+himage Collector mkdir /usr/netflow > /dev/null 2>&1
+himage Collector nfcapd -S 1 -w -t 3600 -l /usr/netflow/ -p 4444 
 # -D za pozadinski proces
+# nfcapd i nfdump bacaju error
 
 
 # Configure Router to send netflow data to collector
-himage Router ngctl mkpeer eth0: netflow lower iface0
-himage Router ngctl name eth0:lower netflow
-himage Router ngctl connect eth0: netflow: upper out0
-himage Router ngctl mkpeer netflow: ksocket export9 inet/dgram/udp
-himage Router ngctl name netflow:export9 exporter
-himage Router ngctl msg netflow: setconfig { iface=0 conf=7 }
-himage Router ngctl msg netflow:export9 connect inet/10.0.3.20:4444
+himage Router ngctl mkpeer eth0: netflow lower iface0 > /dev/null 2>&1
+himage Router ngctl name eth0:lower netflow > /dev/null 2>&1
+himage Router ngctl connect eth0: netflow: upper out0 > /dev/null 2>&1
+himage Router ngctl mkpeer netflow: ksocket export9 inet/dgram/udp > /dev/null 2>&1
+himage Router ngctl name netflow:export9 exporter > /dev/null 2>&1
+himage Router ngctl msg netflow: setconfig { iface=0 conf=7 } > /dev/null 2>&1
+himage Router ngctl msg netflow:export9 connect inet/10.0.3.20:4444 > /dev/null 2>&1
 
 echo "Done"
