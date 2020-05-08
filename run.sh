02e1f76264e5880ab32c2b5a51454317c3d356b1
markovac <tm50053@fer.hr>
2020-05-08 02:14:28 +0200

    Final version

--- run.sh
+++ run.sh
@@ -19,10 +19,10 @@ himage Collector pkg add cache/libtool-2.4.6_1.txz > /dev/null 2>&1
 himage Collector pkg add cache/nfdump-1.6.19.txz > /dev/null 2>&1
 
 himage Collector mkdir /usr/netflow > /dev/null 2>&1
-himage Collector nfcapd -S 1 -w -t 3600 -l /usr/netflow/ -p 4444 
-# -D za pozadinski proces
-# nfcapd i nfdump bacaju error
-
+himage Collector mkdir /usr/netflow/r1 > /dev/null 2>&1
+himage Collector mkdir /usr/netflow/r2 > /dev/null 2>&1
+himage Collector nfcapd -D -w -l /usr/netflow/r1/ -p 4444 > /dev/null 2>&1
+himage Collector nfcapd -D -w -l /usr/netflow/r2/ -p 4445 > /dev/null 2>&1
 
 # Configure Router to send netflow data to collector
 himage Router ngctl mkpeer eth0: netflow lower iface0 > /dev/null 2>&1
@@ -32,5 +32,14 @@ himage Router ngctl mkpeer netflow: ksocket export9 inet/dgram/udp > /dev/null 2
 himage Router ngctl name netflow:export9 exporter > /dev/null 2>&1
 himage Router ngctl msg netflow: setconfig { iface=0 conf=7 } > /dev/null 2>&1
 himage Router ngctl msg netflow:export9 connect inet/10.0.3.20:4444 > /dev/null 2>&1
+
+# Configure Router1 to send netflow data to collector
+himage Router1 ngctl mkpeer eth0: netflow lower iface0 > /dev/null 2>&1
+himage Router1 ngctl name eth0:lower netflow > /dev/null 2>&1
+himage Router1 ngctl connect eth0: netflow: upper out0 > /dev/null 2>&1
+himage Router1 ngctl mkpeer netflow: ksocket export9 inet/dgram/udp > /dev/null 2>&1
+himage Router1 ngctl name netflow:export9 exporter > /dev/null 2>&1
+himage Router1 ngctl msg netflow: setconfig { iface=0 conf=7 } > /dev/null 2>&1
+himage Router1 ngctl msg netflow:export9 connect inet/10.0.3.20:4445 > /dev/null 2>&1
 
 echo "Done"
